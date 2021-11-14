b2cfc3f3d1a37f37871bf469ff7022bd239512ef
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-11-14 12:48:26 +0530

    Retriving top 3 data

---
+++ LinqProject/LinqProject/Management.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace LinqProject
+{
+    class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            var recordData = (from productReviews in listProductReview
+                              orderby productReviews.Rating descending
+                              select productReviews).Take(3);
+
+
+
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductId + " " + "UserID:" + list.UserID +
+                    " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Reveiew + " " +
+                    "People liked:-" + list.isLike);
+            }
+        }
+    }
+}
